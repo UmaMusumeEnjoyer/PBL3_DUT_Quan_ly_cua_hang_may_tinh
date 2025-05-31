1e668d8cef144ba3980c80892418bbfec5fab1bf
hahuy1708 <hahuy17082005@gmail.com>
2025-05-31 11:22:06 +0700

    remove unused directives

--- Gwenchana/DataAccess/DTO/Employee.cs
+++ Gwenchana/DataAccess/DTO/Employee.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Gwenchana.DataAccess.DTO
+﻿namespace Gwenchana.DataAccess.DTO
 {
     public class Employee
     {
